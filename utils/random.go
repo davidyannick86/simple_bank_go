a38566adb5e98627a569bea2a47e41f8b723ed16
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-04-16 16:00:39 +0200

    Update .gitignore to include go.sum; add test task and update dropdb command in Taskfile; implement random utility functions

---
+++ utils/random.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func Init() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1) // min -> max
+}
+
+// random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for range n {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD", "AUD", "JPY"}
+	return currencies[rand.Intn(len(currencies))]
+}
