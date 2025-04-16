8cfef1c7c2b44e06ecdfd9a75b39ed349c6f5c9d
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-04-16 16:40:49 +0200

    Update transfer schema and models to enforce non-nullable to_account_id; add transfer tests

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,84 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/davidyannick86/simple-bank/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        utils.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+	require.WithinDuration(t, transfer.CreatedAt, transfer.CreatedAt, time.Second)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	createRandomTransfer(t, account1, account2)
+}
+
+func TestGetTransfer(t *testing.T) {
+	transfer := createRandomTransfer(t, createRandomAccount(t), createRandomAccount(t))
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer.ID, transfer2.ID)
+	require.Equal(t, transfer.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer.Amount, transfer2.Amount)
+
+	require.WithinDuration(t, transfer.CreatedAt, transfer2.CreatedAt, time.Second)
+}
+
+func TestListTransfers(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t, account1, account2)
+	}
+
+	arg := ListTransfersParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Limit:         5,
+		Offset:        5,
+	}
+
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+		require.WithinDuration(t, transfer.CreatedAt, time.Now(), time.Second)
+	}
+}
