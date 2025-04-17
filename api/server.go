ecc3f5ec0f2ea1b92502a9f17f7c8cc8c9b493d0
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-04-17 15:48:50 +0200

    Add server functionality and account creation endpoint
    - Implemented server startup and connection to PostgreSQL database.
    - Added createAccount handler to process account creation requests.
    - Introduced API documentation for account creation.
    - Updated Taskfile with server task for running the application.

---
+++ api/server.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	db "github.com/davidyannick86/simple-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// serve hhtp requests
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// add routes to the router
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
